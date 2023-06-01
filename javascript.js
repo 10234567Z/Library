64aecf6a027c276e8f3bb8f0b61fa500f2d6a0ef
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-06-01 19:25:32 +0530

    Add form's UI/UX and a pop-up of form upon click of add button

--- javascript.js
+++ javascript.js
@@ -1,26 +1,46 @@
-function Book(bookName,pages,readStatus){
+function Book(bookName, pages, readStatus) {
     this.bookName = bookName
     this.pages = pages
     this.readStatus = readStatus
-    this.info = function(){
+    this.info = function () {
         return `${bookName},${pages} pages,${readStatus}`
     }
 }
 
 let addButton = document.querySelector('.addBook');
 let form = document.querySelector('form');
+let allInputs = document.querySelectorAll('input');
+let processForm = [];
 
-addButton.addEventListener('mousedown' , () => {
+addButton.addEventListener('mousedown', () => {
 
-    if(form.style.display == 'grid'){
+    if (form.style.display === 'grid') {
         form.style.display = 'none';
+        processForm.length = 0;
     }
-    else{
+    else {
         form.style.display = 'grid';
     }
 })
 
 
+document.querySelector('.submitButton').addEventListener('mousedown', () => {
+    for (i = 0; i < allInputs.length; i++) {
+        if (allInputs[i].checkValidity() === true) {
+            processForm.push(true);
+        }
+    }
+    let processBool = processForm.length >= 5 && processForm.every(e => e === processForm[0]);
+    if (processBool) {
+        form.style.display = 'none';
+        processForm.length = 0;
+        AddBook();
+    }
+})
+
+function AddBook() {
+    console.log('reached');
+}
 // let bookName = prompt('Please provide a book name' , '');
 // let pages = prompt('Please provide the no. of pages in the book you want to add' , '');
 // let readStatus = prompt('Did you read the book yet?', '');
@@ -28,3 +48,6 @@ addButton.addEventListener('mousedown' , () => {
 // let newBook = new Book(bookName,pages,readStatus);
 
 // console.log(newBook.info());
+
+
+
